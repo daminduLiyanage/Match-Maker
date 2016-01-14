65814f8607444d84d153abea90c45c38ed34ab1a
Damindu Liyanage <damindu7@gmail.com>
2016-01-14 13:05:38 +0530

    Name Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Welcome to Match Maker</div>
             </div>
         </div>
     </body>
