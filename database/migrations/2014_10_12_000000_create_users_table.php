2b16bbba2a7f853fa42b8a600b06c16ac41bb3a1
Damindu Liyanage <damindu7@gmail.com>
2016-01-19 11:17:31 +0530

    user db tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,37 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username');
             $table->string('password', 60);
+            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('nickname');
+            $table->string('gender');
+            $table->string('interested_in');
+            $table->date('birthday');
+            $table->string('age');
+            $table->string('religion');
+            $table->string('country');
+            $table->string('hometown');
+            $table->string('location');
+            $table->string('address');
+            $table->string('telephone_mobile');
+            $table->string('telephone_home');
+            $table->string('education');
+            $table->string('work');
+            $table->string('languages');
+            $table->string('facebook');
+            $table->string('about');
+            $table->boolean('is_verified');
+            $table->integer('cover_photo');
+            $table->integer('inbox');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
@@ -29,6 +53,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('user');
     }
 }
